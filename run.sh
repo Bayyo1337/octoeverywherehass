6e213f1b01a081f73fb211ab857614d8a00ce205
Bayyo1337 <115353825+Bayyo1337@users.noreply.github.com>
2025-05-12 12:56:51 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Load printer IP from HA add-on options
-PRINTER_IP=$(jq -r '.printer_ip' /data/options.json)
-
-echo "Starting OctoEverywhere in Elegoo Companion mode for IP: $PRINTER_IP"
-
-COMPANION_MODE=elegoo \
-PRINTER_IP="$PRINTER_IP" \
-/app/OctoEverywhere
